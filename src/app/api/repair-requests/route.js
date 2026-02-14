c90548e9b35b9014c4390ff5c30f455e2df83665
Sakib8git <sakib30278@gmail.com>
2026-02-14 09:56:07 +0600

    setup for deploy3

--- src/app/api/repair-requests/route.js
+++ src/app/api/repair-requests/route.js
@@ -1,4 +1,4 @@
-import { getCollection } from "@/lib/dbConntect";
+import { getCollection } from "@/lib/dbConnect";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -1,15 +1,14 @@
-
-import { getCollection } from "@/lib/dbConntect";
+import { getCollection } from "@/lib/dbConnect";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
   try {
     // ১. কালেকশন কানেক্ট করা
     const usersCollection = await getCollection("users");
-    
+
     // ২. ডাটা খুঁজে বের করা
     const result = await usersCollection.find().toArray();
-    
+
     // ৩. রেসপন্স পাঠানো
     return NextResponse.json(result);
   } catch (error) {
@@ -32,10 +31,4 @@ export async function POST(request) {
   }
 }
 
-// const { connect } = require("@/lib/dbConntect");
 
-// export async function GET(request) {
-//   const usersCollection = await connect("users");
-//   const result = await usersCollection.find().toArray();
-//   return Response.json(result);
-// }
